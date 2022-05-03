99616608aa56a3f6d6565909c350349d00544c97
plapis <92473629+plapis@users.noreply.github.com>
2022-05-02 18:29:16 -0600

    Add files via upload

---
+++ PracticaHarry/src/Paquete/Main.java
@@ -0,0 +1,20 @@
+package Paquete;
+
+public class Main {
+    public static void main(String[] args) {
+        Hermione per = new Hermione();
+        per.print();
+
+        Harry per2 = new Harry();
+        per2.print();
+
+        Ron per3 = new Ron();
+        per3.print();
+
+        Dumbledore per4 = new Dumbledore();
+        per4.print();
+
+        Voldemort per5 = new Voldemort();
+        per5.print();
+    }
+}
---
+++ PracticaHarry/src/Paquete/Personajes.java
@@ -0,0 +1,74 @@
+package Paquete;
+
+    class Hermione {
+        public String name = "Hermione";
+        int edad = 14;
+        String house = "Gryffindor";
+        String sex = "Normal";
+
+        void print() {
+            System.out.println("Nombre: " + name);
+            System.out.println("Edad: " + edad);
+            System.out.println("Casa: " + house);
+            System.out.println("Sexo: " + sex);
+            System.out.println();
+        }
+    }
+
+    class Harry {
+        public String name = "Harry";
+        int edad = 14;
+        String house = "Gryffindor";
+        String sex = "Normal";
+
+        void print() {
+            System.out.println("Nombre: " + name);
+            System.out.println("Edad: " + edad);
+            System.out.println("Casa: " + house);
+            System.out.println("Sexo: " + sex);
+            System.out.println();
+        }
+    }
+
+    class Ron {
+        public String name = "Hermione";
+        int edad = 14;
+        String house = "Gryffindor";
+        String sex = "Normal";
+
+        void print() {
+            System.out.println("Nombre: " + name);
+            System.out.println("Edad: " + edad);
+            System.out.println("Casa: " + house);
+            System.out.println("Sexo: " + sex);
+            System.out.println();
+        }
+    }
+    class Dumbledore {
+        public String name = "Dumbledore";
+        int edad = 71;
+        String house = "Ninguno";
+        String sex = "Gay";
+
+        void print() {
+            System.out.println("Nombre: " + name);
+            System.out.println("Edad: " + edad);
+            System.out.println("Casa: " + house);
+            System.out.println("Sexo: " + sex);
+            System.out.println();
+        }
+    }
+    class Voldemort {
+        public String name = "Voldemort";
+        int edad = 71;
+        String house = "Ninguno";
+        String sex = "Normal";
+
+        void print() {
+            System.out.println("Nombre: " + name);
+            System.out.println("Edad: " + edad);
+            System.out.println("Casa: " + house);
+            System.out.println("Sexo: " + sex);
+        }
+    }
+
